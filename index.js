8929e255a7be0409d8ae9d8246640a6617f9efd7
GuWuelta <rm93449@fiap.com.br>
2022-11-23 18:08:24 -0300

    Objeto json sendo criado, falta apenas o a saída.

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-var fs = require('fs');
+const fs = require("fs");
 
-fs.readFile('./data/exemplo.csv', 'utf-8', function (err, data) {
-    if(err) throw err;
-    console.log(data)
-    const lines = data.split('\n');
-    const keys = lines[0].split(',');
-    const dados = lines.slice(1, lines.length - 1).map((linha) => linha.split(","))
-    console.log(lines)
-    console.log(keys)
-    console.log(dados)
-    i = 1
-    while(i < lines.length) {
-        console.log(keys[0] + " e " + keys[1]+ " : " + lines[i])
-        i++
-    }
-    console.log("--------------------------")
-    console.log(keys[0] + ":" + dados[0][0] + "," + keys[1] + ":" + dados[0][1])
+fs.readFile("./data/exemplo.csv", "utf-8", function (err, csv) {
+  if (err) throw err;
+  //console.log(csv)
+  const lines = csv.split("\n");
+  const keys = lines[0].split(",");
+  const data = lines.slice(1).map((linha) => linha.split(","));
+  const arr = [];
 
-    for (const row of dados) {
-        for (const col of row){
-        }
+  for (const row of data) {
+    const jsonObj = {};
+    for (const col in row) {
+      jsonObj[keys[col]] = row[col];
     }
+    arr.push(jsonObj);
+  }
+  console.log(arr);
+
+  return arr;
 });
 
-
-
-
-
-
+fs.writeFile("./output/", JSON.stringify(arr), function (arr) {});
