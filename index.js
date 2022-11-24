0a1a17d34b5a05f4bbd6ddad9422f19feb54a06e
GuWuelta <rm93449@fiap.com.br>
2022-11-24 15:20:15 -0300

    Lógica separada por funções.

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 const fs = require("fs");
 
-fs.readFile("./data/exemplo.csv", "utf-8", function (err, csv) {
-  if (err) throw err;
-  //console.log(csv)
+function readFile() {
+  const csv = fs.readFileSync("./data/exemplo.csv", "utf-8");
+  return csv;
+}
+
+function convertFile(csv) {
+  csv = readFile();
+
   const lines = csv.split("\n");
-  const keys = lines[0].split(",");
+  const headers = lines[0].split(",");
   const data = lines.slice(1).map((linha) => linha.split(","));
+
   const arr = [];
 
   for (const row of data) {
     const jsonObj = {};
     for (const col in row) {
-      jsonObj[keys[col]] = row[col];
+      jsonObj[headers[col]] = row[col];
     }
     arr.push(jsonObj);
   }
-  console.log(arr);
+  return arr;
+}
 
-  fs.writeFileSync("./output/file.json", JSON.stringify(arr));
-});
+function createJsonFile(arr, csv) {
+  arr = convertFile(csv);
+  console.log(arr);
+  return fs.writeFileSync("./output/file.json", JSON.stringify(arr));
+}
+
+createJsonFile();
